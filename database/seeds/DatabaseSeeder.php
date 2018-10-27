66b488ba70a64cf32700256d0eb485976293a038
qiankaihua <qiankaihua@126.com>
2018-10-27 19:35:32 +0800

    add database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $users = $this->getUsers(10);
+        foreach ($users as $user) {
+            $user->save();
+        }
+        $classifications = $this->getClassifications(5);
+        foreach ($classifications as $classification) {
+            $classification->save();
+            $algorithms = $this->getAlgorithms(5);
+            foreach ($algorithms as $algorithm) {
+                $classification->algorithms()->save($algorithm);
+            }
+        }
+    }
+
+    /**
+     * @param int $num
+     * @return \App\Models\User[]
+     */
+    private function getUsers(int $num)
+    {
+        return factory(\App\Models\User::class, $num)->make();
+    }
+
+    /**
+     * @param int $num
+     * @return \App\Models\Classification[]
+     */
+    private function getClassifications(int $num)
+    {
+        return factory(\App\Models\Classification::class, $num)->make();
+    }
+
+    /**
+     * @param int $num
+     * @return \App\Models\Algorithm[]
+     */
+    private function getAlgorithms(int $num)
+    {
+        return factory(\App\Models\Algorithm::class, $num)->make();
     }
 }
