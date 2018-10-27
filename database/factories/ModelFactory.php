9ef537efbc2acaa8a6c527e6efd274489fc4bcbe
qiankaihua <qiankaihua@126.com>
2018-10-27 19:04:28 +0800

    add ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,9 +11,35 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'id' => \PascalDeVink\ShortUuid\ShortUuid::uuid4(),
+        'username' => $faker->userName,
+        'password' => app('hash')->make(\App\Helper::sha256($faker->password())),
+        'realname' => \App\Helper::generateChinese(),
         'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'github' => $faker->userName,
+    ];
+});
+
+$factory->define(\App\Models\Classification::class, function (\Faker\Generator $faker) {
+    return [
+        'name' => $faker->words(3),
+        'description' => $faker->paragraph(),
+    ];
+});
+
+$factory->define(\App\Models\Algorithm::class, function (\Faker\Generator $faker) {
+    return [
+        'name' => $faker->words(3),
+        'pseudoCode' => [$faker->sentence(), $faker->sentence(), $faker->sentence()],
+        'jsCode' => [$faker->sentence(), $faker->sentence(), $faker->sentence()],
+        'CPlusCode' => [$faker->sentence(), $faker->sentence(), $faker->sentence()],
+        'explain' => [$faker->sentence(), $faker->sentence(), $faker->sentence()],
+        'problem' => [[
+            'name' => $faker->words(5),
+            'link' => 'http://www.testOJ.com/' . rand(1, 1000000),
+        ]],
     ];
 });
