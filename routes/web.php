8b1be090a8025c634f2d0857b6541d866d82316a
qiankaihua <qiankaihua@126.com>
2018-10-30 10:44:25 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,73 @@
 |
 */
 
+/**
+ * @var $router \Laravel\Lumen\Routing\Router
+ */
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group([
+    'prefix' => 'auth',
+    'namespace' => 'Auth'
+], function () use ($router) {
+    $router->post('/login', 'Login@handle');
+
+    $router->put('/security/{id}', 'ChangePassword@handle');
+
+    $router->post('/forget', 'ForgetPassword@handle');
+
+    $router->post('/reset', 'ResetPassword@handle');
+});
+
+$router->group([
+    'prefix' => 'user',
+    'namespace' => 'User',
+], function () use ($router) {
+    $router->post('/add', 'Add@handle');
+
+    $router->get('/', 'List@handle');
+
+    $router->get('/{user_id}', 'Detail@handle');
+
+    $router->put('/{user_id}', 'Change@handle');
+});
+
+$router->group([
+    'prefix' => 'classification',
+    'namespace' => 'Classification',
+], function () use ($router) {
+    $router->get('/', 'List@handle');
+
+    $router->post('/', 'Add@handle');
+
+    $router->put('/{id}', 'Change@handle');
+
+    $router->delete('/{id}', 'Delete@handle');
+
+    $router->post('/{id}', 'Restore@handle');
+});
+
+$router->group([
+    'prefix' => 'classification/{id}/algorithm',
+    'namespace' => 'Algorithm',
+], function () use ($router) {
+    $router->get('/', 'List@handle');
+
+    $router->get('/{id}', 'Detail@handle');
+
+    $router->post('/', 'Add@handle');
+
+    $router->put('/{id}/verify', 'Verify@handle');
+
+    $router->put('/{id}', 'Change@handle');
+
+    $router->delete('/{id}', 'Delete@handle');
+
+    $router->post('/{id}', 'Restore@handle');
+
+    $router->post('/{id}/problem', 'AddProblem@handle');
+
+    $router->delete('/{id}/problem', 'DeleteProblem@handle');
 });
