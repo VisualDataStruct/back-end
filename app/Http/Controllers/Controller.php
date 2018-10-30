f8863a41ce54bd8df50999ab74ea65a06e48374f
qiankaihua <qiankaihua@126.com>
2018-10-30 17:56:06 +0800

    add error function to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,16 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @param int $code
+     * @param string $message
+     *
+     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
+     */
+    protected function error (int $code, string $message = '') {
+        return response([
+            'code' => $code,
+            'message' => $message,
+        ], $code);
+    }
 }
