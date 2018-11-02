a51aa02095ecc61ef5441ca4d266513b03860a86
qiankaihua <qiankaihua@126.com>
2018-11-02 16:37:07 +0800

    add login test

---
+++ tests/Auth/LoginTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class LoginTest extends TestCase
+{
+    public function testLoginSuccessWithEmail()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'email' => 'admin@VDS.com',
+            'password' => \App\Helper::sha256('admin'),
+        ]);
+        $this->assertResponseOk();
+        $this->seeInDatabase('api_token',
+            [
+                'user_id' => '1',
+                'token' => $response->original['token'],
+            ]
+        );
+    }
+    public function testLoginSuccessWithUsername()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'username' => 'administrator',
+            'password' => \App\Helper::sha256('admin'),
+        ]);
+        $this->assertResponseOk();
+        $this->seeInDatabase('api_token',
+            [
+                'user_id' => '1',
+                'token' => $response->original['token'],
+            ]
+        );
+    }
+    public function testLoginFailWithoutEmailNorUsername()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'password' => \App\Helper::sha256('admin'),
+        ]);
+        $this->assertEquals(422, $response->status());
+    }
+    public function testLoginFailWithoutPassword()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'email' => 'admin@VDS.com',
+        ]);
+        $this->assertEquals(422, $response->status());
+    }
+    public function testLoginFailWithEmailNotInDatabase()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'email' => 'notExist@unknown.com',
+            'password' => \App\Helper::sha256('admin'),
+        ]);
+        $this->assertEquals(404, $response->status());
+    }
+    public function testLoginFailWithUsernameNotInDatabase()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'username' => 'notRegister',
+            'password' => \App\Helper::sha256('admin'),
+        ]);
+        $this->assertEquals(404, $response->status());
+    }
+    public function testLoginFailWithErrorPassword()
+    {
+        $response = $this->call('POST', '/auth/login', [
+            'email' => 'admin@VDS.com',
+            'password' => \App\Helper::sha256('admin1'),
+        ]);
+        $this->assertEquals(401, $response->status());
+    }
+}
