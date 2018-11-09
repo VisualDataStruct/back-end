1f42af4f6c15d203e1728c8f766838c32e5bbf66
qiankaihua <qiankaihua@126.com>
2018-11-09 21:29:28 +0800

    update annotation

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -9,7 +9,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Models
  *
- * @method static \Illuminate\Database\Eloquent\Builder|static where($col, $eq='=', $value='')
+ * @method static \Illuminate\Database\Eloquent\Builder|static where(string $col, string $eq='=', $value='')
+ * @method static \Illuminate\Database\Eloquent\Builder|static offset(integer $val)
+ * @method static \Illuminate\Database\Eloquent\Builder|static limit(integer$val)
  * @method static static find($id)
  * @method static static first()
  */
