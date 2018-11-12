22554d538d448dfe1067247d97624c7949d82080
qiankaihua <qiankaihua@126.com>
2018-11-12 16:23:42 +0800

    add change classification test

---
+++ tests/Controller/Classification/ChangeClassificationTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ChangeClassificationListTest extends TestCase
+{
+    public function testChangeClassificationSuccess()
+    {
+        $user = \App\Models\User::first();
+        $classification = \App\Models\Classification::first();
+        $test_classification = [
+            'name' => \PascalDeVink\ShortUuid\ShortUuid::uuid4(),
+            'description' => 'test description' . \PascalDeVink\ShortUuid\ShortUuid::uuid4(),
+        ];
+        $this->actingAs($user)->call('PUT',  '/classification/' . $classification->id, $test_classification);
+        $this->assertResponseOk();
+        $test_classification['id'] = $classification->id;
+        $this->seeInDatabase('classification', $test_classification);
+    }
+    public function testChangeClassificationFailWithNoLogin()
+    {
+        $test_classification = [
+            'name' => 'test',
+            'description' => 'test description',
+        ];
+        $classification = \App\Models\Classification::first();
+        $response = $this->call('PUT', '/classification/' . $classification->id, $test_classification);
+        $this->assertEquals(401, $response->status());
+    }
+    public function testChangeClassificationFailWithExistName()
+    {
+        $user = \App\Models\User::first();
+        $classification = \App\Models\Classification::first();
+        $test_classification = [
+            'name' => $classification->name,
+            'description' => 'test description',
+        ];
+        $response = $this->actingAs($user)->call('PUT', '/classification/' . $classification->id, $test_classification);
+        $this->assertEquals(422, $response->status());
+    }
+    public function testChangeClassificationFailWithNotFound()
+    {
+        $user = \App\Models\User::first();
+        $response = $this->actingAs($user)->call('PUT', '/classification/0', [
+            'name' => 'test',
+            'description' => 'test description',
+        ]);
+        $this->assertEquals(404, $response->status());
+    }
+}
