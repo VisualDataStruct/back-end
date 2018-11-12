0b047669e88709b7bb92ac6c6fec4e59bec7e284
qiankaihua <qiankaihua@126.com>
2018-11-12 16:27:53 +0800

    add restore classification test

---
+++ tests/Controller/Classification/RestoreClassificationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class RestoreClassificationTest extends TestCase
+{
+    public function testRestoreClassificationSuccess()
+    {
+        $user = \App\Models\User::first();
+        $classification = \App\Models\Classification::onlyTrashed()->first();
+        if ($classification === null) {
+            $classification = \App\Models\Classification::first();
+            $classification->delete();
+        }
+        $this->actingAs($user)->call('POST',  '/classification/' . $classification->id);
+        $this->assertResponseOk();
+        $this->seeInDatabase('classification', [
+            'id' => $classification->id,
+            'deleted_at' => null,
+        ]);
+    }
+    public function testRestoreClassificationFailWithNotFound()
+    {
+        $user = \App\Models\User::first();
+        $response = $this->actingAs($user)->call('POST',  '/classification/0');
+        $this->assertEquals(404, $response->status());
+    }
+    public function testDeleteClassificationFailWithNoLogin()
+    {
+        $classification = \App\Models\Classification::onlyTrashed()->first();
+        $response = $this->call('POST',  '/classification/' . $classification->id);
+        $this->assertEquals(401, $response->status());
+    }
+}
