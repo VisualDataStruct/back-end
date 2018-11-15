b38f26f7f92ff289b8d3dbc617ff27635e7c3750
qiankaihua <qiankaihua@126.com>
2018-11-15 13:54:44 +0800

    add algorithm list test

---
+++ tests/Controller/Algorithm/GetAlgorithmListTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class GetAlgorithmListTest extends TestCase
+{
+    public function testAlgorithmListSuccessWithNoLogin()
+    {
+        $classification = \App\Models\Classification::first();
+        $algorithms = $classification->algorithms;
+        $algorithmCount = 0;
+        foreach ($algorithms as $algorithm) {
+            if ($algorithm->isPassed) {
+                $algorithmCount += 1;
+            }
+        }
+        $response = $this->call('GET', '/classification/' . $classification->id . '/algorithm');
+        $this->assertResponseOk();
+        $response_algorithms = $response->original['algorithms'];
+        $this->assertEquals($algorithmCount, count($response_algorithms));
+        $this->assertEquals($algorithmCount, $response->original['sum']);
+        foreach ($response_algorithms as $response_algorithm) {
+            $this->seeInDatabase('algorithm', $response_algorithm);
+        }
+    }
+    public function testAlgorithmListSuccessWithLogin()
+    {
+        $user = \App\Models\User::first();
+        $classification = \App\Models\Classification::onlyTrashed()->first();
+        if ($classification === null) {
+            $classification = \App\Models\Classification::first();
+        }
+        $algorithmCount = $classification->algorithms()->withTrashed()->count();
+        $response = $this->actingAs($user)->call('GET', '/classification/' . $classification->id . '/algorithm');
+        $this->assertResponseOk();
+        $response_algorithms = $response->original['algorithms'];
+        $this->assertEquals($algorithmCount, count($response_algorithms));
+        foreach ($response_algorithms as $response_algorithm) {
+            if (isset($response_algorithm['deleted_at'])) {
+                unset($response_algorithm['deleted_at']);
+            }
+            $this->seeInDatabase('algorithm', $response_algorithm);
+        }
+    }
+    public function testAlgorithmListFailWithNotFoundClassification()
+    {
+        $classification = \App\Models\Classification::onlyTrashed()->first();
+        $response = $this->call('GET', '/classification/' . ($classification->id ?? 0) . '/algorithm');
+        $this->assertEquals(404, $response->status());
+    }
+}
